d1aa9fc16a082e568ff671a31f875e69267e70a7
quannguyen2306-ctrl <65434894+quannguyen2306-ctrl@users.noreply.github.com>
2022-03-01 10:27:36 +0700

    8

---
+++ sound.js
@@ -0,0 +1,10 @@
+// var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+// if (!isChrome){
+//     $('#iframeAudio').remove()
+// }
+// else {
+//     $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+// }
+
+// document.getElementById("musicplayer").play()
+
